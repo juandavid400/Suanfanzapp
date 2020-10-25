c8e9003950c8eb8f41a2815dc464965a98dd4044
camiloruiz27 <camiloruiz27@hotmail.com>
2020-10-25 11:59:10 -0500

    Incluyendo Index

--- src/app/shared/services/chat/chat.service.ts
+++ src/app/shared/services/chat/chat.service.ts
@@ -14,7 +14,7 @@ export class ChatService {
 
   connect() {
     return new Observable(observer => {
-      this.socket = io('localhost:3000');
+      this.socket = io('https://d51802264cfb.ngrok.io');
       this.socket.on('connect', () => {
         observer.next();
       })
